b651337bec6f6bee889e691f559b44417654e9ba
Veaceslav Falico <vfalico@gmail.com>
2014-12-19 15:21:10 +0100

    sched: don't change the cpu
    It's a nasty bug when, while the cpumask is void, the cpumask_first()
    returns something non-zero.
    
    Needs to be addressed later.
    
    Signed-off-by: Veaceslav Falico <vfalico@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5970,8 +5970,9 @@ static int get_group(int cpu, struct sd_data *sdd, struct sched_group **sg)
 	struct sched_domain *sd = *per_cpu_ptr(sdd->sd, cpu);
 	struct sched_domain *child = sd->child;
 
-	if (child)
-		cpu = cpumask_first(sched_domain_span(child));
+	/* WTF is that? XXX sched_domain_span returns void cpumask */
+//	if (child)
+//		cpu = cpumask_first(sched_domain_span(child));
 
 	if (sg) {
 		*sg = *per_cpu_ptr(sdd->sg, cpu);
