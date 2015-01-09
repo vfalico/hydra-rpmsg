69a6578b3e54ac2d2641603d7c1c1e81ec593081
ajopanoor <ajopanoor@gmail.com>
2015-01-09 17:36:34 +0100

    ioctl header for rpmsg client programs.
    Signed-off-by Ajo Jose Panoor <ajopanoor@gmail.com>

---
+++ samples/rpmsg/rpmsg_client_ioctl.h
@@ -0,0 +1,9 @@
+#ifndef _RPMSG_CLIENT_IOCTL_H_
+#define _RPMSG_CLIENT_IOCTL_H_
+#include <linux/types.h>
+
+#define RPMSG_CLIENT_DUMMY_IOCTL _IOWR('s', 1, void *)
+
+//#define RPMSG_CLIENT_PERF_TEST_START _IOWR('s', 1, void *)
+
+#endif //_RPMSG_CLIENT_IOCTL_H_
